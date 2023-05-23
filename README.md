1cb632d54bc8290a2b64b7d62b4c59c47fe9f8af
MatheusLopes13 <matheuuslopes13@gmail.com>
2023-05-22 21:04:38 -0300

    front-end-crud

---
+++ README.md
@@ -0,0 +1,3 @@
+apos baixar o projeto:
+1: rodar o comando npm install.
+2: abrir o index.html no navegador.
--- src/script.js
+++ src/script.js
@@ -1,6 +1,10 @@
 
+let modal;
+let selecionarObj;
+const idArray = ['nomeModal', 'ruaModal', 'numeroModal', 'bairroModal', 'cidadeModal', 'estadoModal']
 
 window.onload = () => {
+
 
     buscarEmpresas()
 
@@ -9,12 +13,29 @@ window.onload = () => {
 
         fetch(url)
             .then(response => response.json())
-            .then(empresas => empresas.forEach(item => criarTabela(item)))
+            .then((empresas) => {
+                if (empresas.length > 0) {
+                    empresas.forEach(item => criarTabela(item))
+                }
+                else {
+                    const meuContainer = document.getElementById("mensagemEmpresa")
+                    let p_ = document.createElement("p");
+                    p_.innerText = "Ainda não há empresas cadastradas"
+                    meuContainer.appendChild(p_)
+                }
+                }
+            )
     }
+
+
 }
 
 
 function criarTabela(obj) {
+    const botaoEditar = document.createElement('button')
+    const botaoDetalhes = document.createElement('button')
+    const botaoExcluir = document.createElement('button')
+
     const table = document.getElementById('table');
 
     const newTr = document.createElement('tr');
@@ -26,32 +47,174 @@ function criarTabela(obj) {
     td_2.innerText = obj.nome
 
     const td_3 = document.createElement('td');
-    td_3.innerText = obj.endereco
+    td_3.innerText = obj.rua
 
-    const botaoEditar = document.createElement('button')
-    botaoEditar.innerText = "editar"
+    const empresa = [td_1, td_2, td_3, botaoDetalhes, botaoEditar, botaoExcluir]
 
-    const botaoExcluir = document.createElement('button')
-    botaoExcluir.innerText = "excluir"
-    botaoExcluir.addEventListener("click", () => excluirEmpresa(obj.id) )
+    botaoDetalhes.innerText = "Detalhes"
+    botaoDetalhes.addEventListener('click', () => {
+        selecionarObj = obj
+
+        new bootstrap.Modal(modal).show()
+        preencherModal(selecionarObj)
+        habilitaDesabilita(false)
 
 
-    const empresa = [td_1, td_2, td_3, botaoEditar, botaoExcluir]
+    })
+
 
     empresa.forEach(item => newTr.appendChild(item))
 
     table.appendChild(newTr)
 
 
+
+    botaoEditar.innerText = "Editar"
+    modal = document.getElementById("exampleModalEditar");
+    botaoEditar.addEventListener("click", () => {
+
+        selecionarObj = obj
+        new bootstrap.Modal(modal).show()
+        preencherModal(selecionarObj)
+        habilitaDesabilita(true)
+
+
+    })
+
+    botaoExcluir.innerText = "Excluir"
+    botaoExcluir.addEventListener("click", () => {
+        const confirmarExclusao = confirm("Deseja realmente excluir essa empresa?")
+
+        if (confirmarExclusao) {
+            excluirEmpresa(obj.id)
+        }
+    })
+}
+
+function habilitaDesabilita(desabilitado) {
+
+    if (desabilitado) {
+        idArray.forEach((item) => {
+            document.getElementById(item).disabled = false;
+            document.getElementById("botaoAtualizar").style.display = "inline"
+            document.getElementById("botaoCancelar").style.display = "inline"
+
+        })
+
+    }
+    else {
+        idArray.forEach((item) => {
+            document.getElementById(item).disabled = true;
+            document.getElementById("botaoAtualizar").style.display = "none"
+            document.getElementById("botaoCancelar").style.display = "none"
+
+
+        })
+    }
+}
+
+function preencherModal(obj) {
+    document.getElementById("idModal").value = obj.id;
+    document.getElementById("nomeModal").value = obj.nome;
+    document.getElementById("ruaModal").value = obj.rua;
+    document.getElementById("numeroModal").value = obj.numero;
+    document.getElementById("bairroModal").value = obj.bairro;
+    document.getElementById("cidadeModal").value = obj.cidade;
+    document.getElementById("estadoModal").value = obj.estado;
+
 }
 
 
- function excluirEmpresa(id) {
+function criarEmpresa() {
+    const nome = document.getElementById('nomeInput').value;
+    const rua = document.getElementById('ruaInput').value;
+    const numero = Number(document.getElementById('numeroInput').value)
+    const bairro = document.getElementById('bairroInput').value;
+    const cidade = document.getElementById('cidadeInput').value;
+    const estado = document.getElementById('estadoInput').value;
 
+    const novaEmpresa = {
+        nome,
+        rua,
+        numero,
+        bairro,
+        cidade,
+        estado
+    }
+    if (novaEmpresa.nome && novaEmpresa.rua && novaEmpresa.numero && novaEmpresa.bairro && novaEmpresa.cidade && novaEmpresa.estado) {
+        adicionarEmpresa(novaEmpresa)
+
+    }
+    else {
+        alert("Todos os campos devem estar preenchidos")
+    }
+
+}
+
+
+
+function atualizarCadastro() {
+    const id = document.getElementById('idModal').value
+    const nome = document.getElementById('nomeModal').value;
+    const rua = document.getElementById('ruaModal').value;
+    const numero = Number(document.getElementById('numeroModal').value)
+    const bairro = document.getElementById('bairroModal').value;
+    const cidade = document.getElementById('cidadeModal').value;
+    const estado = document.getElementById('estadoModal').value;
+
+
+    const novaEmpresa = {
+        id,
+        nome,
+        rua,
+        numero,
+        bairro,
+        cidade,
+        estado
+    }
+    if (novaEmpresa.nome && novaEmpresa.rua && novaEmpresa.numero && novaEmpresa.bairro && novaEmpresa.cidade && novaEmpresa.estado) {
+        editarEmpresa(novaEmpresa)
+    }
+    else {
+        alert("Todos os campos devem estar preenchidos")
+    }
+
+}
+
+function adicionarEmpresa(obj) {
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(obj),
+    };
+
+    fetch("http://localhost:3000/criar/empresa", options)
+        .then(response => response.json())
+        .then(empresas => console.log(empresas))
+    window.location.reload(true);
+}
+
+function editarEmpresa(obj) {
+    const options = {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(obj),
+    };
+
+    fetch("http://localhost:3000/editar", options)
+        .then(response => response.json())
+        .then(empresas => console.log(empresas))
+    window.location.reload(true);
+}
+
+function excluirEmpresa(id) {
 
     fetch("http://localhost:3000/deletar/" + id, { method: 'DELETE' })
-         .then(response => response.json())
-         .then(empresas => console.log(empresas))
-         window.location.reload(true);
-
- }
+        .then(response => response.json())
+        .then(empresas => console.log(empresas))
+    window.location.reload(true);
+}
